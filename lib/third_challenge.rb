38893f5dfd38f8927c9eb17577fe473a96bea4de
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-29 18:15:11 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
       :hero => {
         #your key/value pairs here
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
@@ -32,6 +32,9 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
